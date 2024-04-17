cd8c36d4c160b64eb0f07c78011e13aaf2459b82
soso akemi <sophiaakemi1@gmail.com>
2024-04-17 00:25:12 +0000

    javascript do agradecimento

---
+++ pages/agradecimento.js
@@ -0,0 +1,7 @@
+function pegarNota (){
+    let nota = localStorage.getItem("nota")
+
+    let paragrafoNota = document.querySelector(".paragrafo_nota")
+
+    paragrafoNota.innerHTML = " Você selecionou ${nota} de 5"
+}
